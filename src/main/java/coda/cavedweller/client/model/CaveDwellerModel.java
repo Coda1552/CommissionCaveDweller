0c5ad2a6de5bdbb332241d4f7dd1928e177f2a66
Coda <cody.koelliker@outlook.com>
2022-04-12 09:09:48 -0600

    added head turning

--- src/main/java/coda/cavedweller/client/model/CaveDwellerModel.java
+++ src/main/java/coda/cavedweller/client/model/CaveDwellerModel.java
@@ -3,7 +3,11 @@ package coda.cavedweller.client.model;
 import coda.cavedweller.CaveDweller;
 import coda.cavedweller.common.entities.CaveDwellerEntity;
 import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.Nullable;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
+import software.bernie.geckolib3.model.provider.data.EntityModelData;
 
 public class CaveDwellerModel extends AnimatedGeoModel<CaveDwellerEntity> {
 
@@ -20,5 +24,20 @@ public class CaveDwellerModel extends AnimatedGeoModel<CaveDwellerEntity> {
     @Override
     public ResourceLocation getAnimationFileLocation(CaveDwellerEntity animatable) {
         return new ResourceLocation(CaveDweller.MOD_ID, "animations/cavedweller.animation.json");
+    }
+
+    @Override
+    public void setLivingAnimations(CaveDwellerEntity entity, Integer uniqueID, @Nullable AnimationEvent customPredicate) {
+        super.setLivingAnimations(entity, uniqueID, customPredicate);
+
+        IBone neck = this.getAnimationProcessor().getBone("neck");
+        IBone head = this.getAnimationProcessor().getBone("head");
+        EntityModelData extraData = (EntityModelData) customPredicate.getExtraDataOfType(EntityModelData.class).get(0);
+
+        neck.setRotationX(extraData.headPitch * ((float) Math.PI / 270F));
+        neck.setRotationY(extraData.netHeadYaw * ((float) Math.PI / 270F));
+
+        head.setRotationX(extraData.headPitch * ((float) Math.PI / 180F));
+        head.setRotationY(extraData.netHeadYaw * ((float) Math.PI / 180F));
     }
 }
